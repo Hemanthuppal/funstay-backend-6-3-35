ae71a65d805e6c284602278014eb0960026bcd02
sharvani67 <sharvanikokkonda@gmail.com>
2025-06-25 11:45:06 +0530

    added tag route modified customertable route

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
 
 
-const db = mysql.createConnection({
-  host: 'localhost', // Your database host
-  user: 'nodeuser', // Your database user
-  password: 'Root@1234', // Your database password
-  database: 'funstay_db', // Your database name
-});
-
 // const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '', // Replace with your DB password
-//   database: 'funstay', // Replace with your DB name
+//   host: 'localhost', // Your database host
+//   user: 'nodeuser', // Your database user
+//   password: 'Root@1234', // Your database password
+//   database: 'funstay_db', // Your database name
 // });
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', // Replace with your DB password
+  database: 'funstay', // Replace with your DB name
+});
 
 // const db = mysql.createConnection({
 //   host: 'localhost',
